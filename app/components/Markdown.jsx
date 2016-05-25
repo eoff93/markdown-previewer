cb312e41270e9b1811117c84a8349492552aba37
eoff <bane.dev101@gmail.com>
2016-05-25 18:44:47 +0200

    Fix 'this' binding so that markdown updates

--- app/components/Markdown.jsx
+++ app/components/Markdown.jsx
@@ -8,6 +8,8 @@ export default class Markdown extends React.Component {
     this.state = {
       value: 'Heading\n=======\n\nSub-heading\n-----------\n \n### Another deeper heading\n \nParagraphs are separated\nby a blank line.\n\nLeave 2 spaces at the end of a line to do a  \nline break\n\nText attributes *italic*, **bold**, \n`monospace`, ~~strikethrough~~ .\n\nShopping list:\n\n  * apples\n  * oranges\n  * pears\n\nNumbered list:\n\n  1. apples\n  2. oranges\n  3. pears\n\nThe rain---not the reign---in\nSpain.\n\n *[Herman Fassett](https://freecodecamp.com/hermanfassett)*',
     };
+
+    this.handleChange = this.handleChange.bind(this);
   }
 
   handleChange() {
