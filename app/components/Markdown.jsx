292ef1885ca2e1ee4cb0d20c42c284712b2fde2c
eoff <bane.dev101@gmail.com>
2016-05-25 18:43:38 +0200

    Refactor react component into ES6 class

--- app/components/Markdown.jsx
+++ app/components/Markdown.jsx
@@ -2,42 +2,48 @@
 import React from 'react';
 import marked from 'marked';
 
-var Markdown = React.createClass({
-  getInitialState: function() {
-    return {
-      value: 'Heading\n=======\n\nSub-heading\n-----------\n \n### Another deeper heading\n \nParagraphs are separated\nby a blank line.\n\nLeave 2 spaces at the end of a line to do a  \nline break\n\nText attributes *italic*, **bold**, \n`monospace`, ~~strikethrough~~ .\n\nShopping list:\n\n  * apples\n  * oranges\n  * pears\n\nNumbered list:\n\n  1. apples\n  2. oranges\n  3. pears\n\nThe rain---not the reign---in\nSpain.\n\n *[Herman Fassett](https://freecodecamp.com/hermanfassett)*'
+export default class Markdown extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      value: 'Heading\n=======\n\nSub-heading\n-----------\n \n### Another deeper heading\n \nParagraphs are separated\nby a blank line.\n\nLeave 2 spaces at the end of a line to do a  \nline break\n\nText attributes *italic*, **bold**, \n`monospace`, ~~strikethrough~~ .\n\nShopping list:\n\n  * apples\n  * oranges\n  * pears\n\nNumbered list:\n\n  1. apples\n  2. oranges\n  3. pears\n\nThe rain---not the reign---in\nSpain.\n\n *[Herman Fassett](https://freecodecamp.com/hermanfassett)*',
     };
-  },
-  handleChange: function() {
+  }
+
+  handleChange() {
     this.setState({
-      value: this.refs.textarea.value
+      value: this.refs.textarea.value,
     });
-  },
-  rawMarkup: function() {
+  }
+
+  rawMarkup() {
     return {
       __html: marked(this.state.value, {
-        sanitize: true
-      })
+        sanitize: true,
+      }),
     };
-  },
-  render: function() {
+  }
+
+  render() {
     return (
       <div className="row">
         <div className="col-md-6">
-          <textarea rows="26" type="text"
-          onChange={this.handleChange}
-          ref="textarea"
-          defaultValue={this.state.value} className="form-control markdown"/>
+          <textarea
+            rows="26"
+            type="text"
+            onChange={this.handleChange}
+            ref="textarea"
+            defaultValue={this.state.value}
+            className="form-control markdown"
+          />
         </div>
         <div className="col-md-6">
           <div
-          className="content"
-          dangerouslySetInnerHTML={this.rawMarkup()}
-        />
+            className="content"
+            dangerouslySetInnerHTML={this.rawMarkup()}
+          />
         </div>
       </div>
     );
   }
-});
-
-export default Markdown;
+}
